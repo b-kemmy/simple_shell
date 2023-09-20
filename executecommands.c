5ed3728fbc92a4497944e66db0c91fe197f17d45
b-kemmy <beatricegesisa@gmail.com>
2023-09-20 07:29:55 -0700

    codes Beatrice

---
+++ executecommands.c
@@ -0,0 +1,46 @@
+#include "shell.h"
+/**
+* executeCommands - Execute the user input as shell commands.
+* @input: The user input string.
+*/
+void executeCommands(const char *input)
+{
+char *token;
+char *commands[100];
+int num_commands = 0;
+token = strtok((char *)input, " ");
+
+while (token != NULL)
+{
+commands[num_commands] = token;
+num_commands++;
+token = strtok(NULL, " ");
+}
+commands[num_commands] = NULL;
+pid_t child_pid = fork();
+
+if (child_pid == -1)
+{
+perror("fork");
+return;
+}
+if (child_pid == 0)
+{
+if (execvp(commands[0], commands) == -1)
+{
+perror("execvp");
+exit(EXIT_FAILURE);
+}
+}
+else
+{
+int status;
+waitpid(child_pid, &status, 0);
+
+if (WIFEXITED(status))
+{
+int exit_status = WEXITSTATUS(status);
+printf("Command exited with status %d\n", exit_status);
+}
+}
+}
---
+++ main.c
@@ -0,0 +1,135 @@
+#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point of the shell program.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *input = NULL;
+
+	while (1)
+	{
+		displayPrompt("Cisfun$\n");
+		input = readInput();
+
+		if (input == NULL)
+		{
+			perror("my_getline");
+			break;
+		}
+
+		handleInput(input);
+		free(input);
+	}
+
+	return (0);
+}
+
+/**
+ * displayPrompt - Display the shell prompt.
+ * @prompt: The prompt string to display.
+ */
+void displayPrompt(const char *prompt)
+{
+	for (int i = 0; prompt[i] != '\0'; i++)
+	{
+		_putchar(prompt[i]);
+	}
+}
+
+/**
+ * readInput - Read user input from stdin.
+ *
+ * Return: A dynamically allocated string containing the user input.
+ */
+char *readInput()
+{
+	char *input = NULL;
+	size_t input_size = 0;
+
+	if (my_getline(&input, &input_size, stdin) == -1)
+	{
+		perror("my_getline");
+		return (NULL);
+	}
+
+	char *new_line = strchr(input, '\n');
+
+	if (new_line)
+	{
+		*new_line = '\0';
+	}
+
+	if (strlen(input) == 0)
+	{
+		return (NULL);
+	}
+
+	return (input);
+}
+
+/**
+ * handleInput - Handle the user input.
+ * @input: The user input string.
+ */
+void handleInput(char *input)
+{
+	if (strstr(input, "|") != NULL)
+	{
+		executePipeCommand(input);
+	}
+	else
+	{
+		executeCommands(input);
+	}
+}
+
+/**
+ * executePipeCommand - Execute a command with pipe.
+ * @command: The command to execute with pipe.
+ */
+void executePipeCommand(const char *command)
+{
+	int pipefd[2];
+	pid_t child_pid;
+	ssize_t read_bytes;
+	char buffer[4096];
+
+	if (pipe(pipefd) == -1)
+	{
+		perror("pipe");
+		return;
+	}
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("fork");
+		return;
+	}
+
+	if (child_pid == 0)
+	{
+		close(pipefd[0]);
+		dup2(pipefd[1], STDOUT_FILENO);
+		system(command);
+		close(pipefd[1]);
+		exit(EXIT_SUCCESS);
+	}
+	else
+	{
+		close(pipefd[1]);
+		while ((read_bytes = read(pipefd[0], buffer, sizeof(buffer))) > 0)
+		{
+			write(STDOUT_FILENO, buffer, read_bytes);
+		}
+		close(pipefd[0]);
+		wait(NULL);
+	}
+}
+
