8a78ee2254b1c3c5759ce620d29af584cda73bba
b-kemmy <beatricegesisa@gmail.com>
2023-09-21 02:25:52 -0700

    Newchanges 21/09/23

--- printenv.c
+++ printenv.c
@@ -5,10 +5,11 @@
 * printEnvironment - Prints the environment variables
 */
 void printEnvironment(void)
+int i;
 {
 char **env_var = environ; /** Access the environment variables*/
 
-for (int i = 0; env_var[i] != NULL; i++)
+for (i = 0; env_var[i] != NULL; i++)
 {
 char *env = env_var[i];
 while (*env)
